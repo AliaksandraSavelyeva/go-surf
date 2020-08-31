c8b322acd6247526bbaeabccc664121e92c9bec6
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2020-08-31 23:40:24 +0300

    header arrows

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,9 @@ $(function() {
 
     $(".header__slider").slick({
         infinite: true,
-        fade: true
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="Left Arrow">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="Right Arrow">'
     });
 
 });
