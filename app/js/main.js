7c21c6b13d675c79017956db0824fe1f5266b3cd
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2020-09-01 23:02:41 +0300

    header dots slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
 $(function() {
 
-    $(".header__slider").slick({
+    $('.header__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="Left Arrow">',
-        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="Right Arrow">'
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="Right Arrow">',
+        asNavFor: '.slider-dots'
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
     });
 
 });
