0f044f7266bcc31937e486bb0851c6a28093e649
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2020-09-03 20:24:43 +0300

    surf section slider

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,11 @@ $(function() {
         asNavFor: '.header__slider'
     });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="Left Arrow">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="Right Arrow">'
+    });
+
 });
