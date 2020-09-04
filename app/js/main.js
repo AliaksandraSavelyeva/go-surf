8e4d2a7588b76e4b46c75b2b908c7194b543886c
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2020-09-04 23:34:01 +0300

    travel section slider

--- app/js/main.js
+++ app/js/main.js
@@ -30,4 +30,11 @@ $(function() {
         focusOnSelect: true
     });
 
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="Left Arrow">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="Right Arrow">',
+    });
+
 });
